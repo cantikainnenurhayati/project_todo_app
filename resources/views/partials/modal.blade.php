0c0783e53ed8fe71c346e4b90d154c8f82b1009e
cantikaine/cantikaine <2223588.cantika@smkn-2sbg.sch.id>
2025-02-12 09:28:19 +0700

    menmabhkan detail agar tidak kosong

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="addListModal" tabindex="-1" aria-labelledby="addListModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        {{--digunakan untuk mengarahkan data ke validasi store  --}}
+        {{--from action di gunakan untuk mengarahkan data ke palidasi store --}}
         <form action="{{ route('lists.store') }}" method="POST" class="modal-content">
             @method('POST')
             @csrf
@@ -38,6 +38,28 @@
                     <label for="name" class="form-label">Nama</label>
                     <input type="text" class="form-control" id="name" name="name"
                         placeholder="Masukkan nama list">
+                </div>
+                <div class="mb-3">
+                    {{--mb-3 dibuat untuk margin bottom atau memberi jarak agar tidak berdekatan--}}
+                    <label for="description" class="form-label">Deskripsi :</label>
+                    {{--form-label digunakan untuk menyamakan ukuran input--}}
+                    {{--label adalah element yang digunakan untuk memberikan informasi tambahkan--}}
+                    <input type="text" class="form-control" id="description" name="description"
+                    {{--input type="text" digunakan untuk menampilkan form yang akan diisi--}}
+                    {{--class form control untuk menyamakan ukuran--}}
+                    {{--id description untuk tanda pengenal--}}
+                        placeholder="Apakah deskripsi kegiatan anda??🤔">
+                        {{--placeholder adalah atribut dalam input yang memberikan teks petunjuk dalam kolom input--}}
+                </div>
+                <div class="col-md-12 mb-3">
+                    <label for="priority" class="form-label">Priority :</label>
+                    <select class="form-control" name="priority" id="taskListId" name="list_id" required>
+                        {{--select adalah menu drop down yang memungkinkan pengguna memiiih satu atau beberapa opsi--}}
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                        <option value="low">Low</option>
+                        {{--option adalah element yang digunakan untuk mendefinisikan pilihan atau menu dari drop down select--}}
+                    </select>    
                 </div>
             </div>
             <div class="modal-footer">
