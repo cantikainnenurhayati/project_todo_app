ff5c57ae8b30d5c730909c5684e785d5983607ca
cantikaine/cantikaine <2223588.cantika@smkn-2sbg.sch.id>
2025-02-12 09:29:17 +0700

    menambhkan komentar

--- app/Http/Controllers/TaskListController.php
+++ app/Http/Controllers/TaskListController.php
@@ -7,19 +7,28 @@ use Illuminate\Http\Request;
 
 class TaskListController extends Controller
 {
-    public function store(Request $request) {
-        $request->validate([
+    public function store(Request $request)
+    // sebuah metode contoroler yg dugunkan untuk menangani permintaan request
+     {
+        $request->validate
+        // digunakan untuk memvalidasi data nama  //
+        ([
             'name' => 'required|max:100'
         ]);
 
-        TaskList::create([
+        TaskList::create
+        // digunakan untuk membuat tasklist berdasarkan nama //
+        ([
             'name' => $request->name
         ]);
 
         return redirect()->back();
+        // digunkan untuk mengembalikan ke halaman awal //
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    // digunakan untuk menghapus tasklist //
+     {
         TaskList::findOrFail($id)->delete();
 
         return redirect()->back();
