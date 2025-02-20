d489e5ae8e1cfccdedeb21adf6b34b82332611d3
cantikaine/cantikaine <2223588.cantika@smkn-2sbg.sch.id>
2025-02-20 08:41:04 +0700

    menambahkan komentar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-// // berfungsi untuk mengimpor task controler,tasklist controler, ke dalam file routes/web.php, controller lain, atau file Blade (jika dibutuhkan).
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
@@ -9,13 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TaskController::class, 'index'])->name('home');
 
 Route::resource('lists', TaskListController::class);
-// menampilkan nama tugas beserta nama daftar tempat tugas tersebut berada, dengan tampilan yang rapi dan responsif.//
 
 Route::resource('tasks', TaskController::class);
-// membuat semua rute standar untuk resource tasks, yang dikendalikan oleh task controller //
-
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
-// untuk menangani pembaruan status tugas menjadi "selesai" (atau "complete") //
-  
 Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
-// untuk memindahkan suatu tugas (task) ke daftar (list) yang berbeda dalam sistem manajemen tugas.//
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
